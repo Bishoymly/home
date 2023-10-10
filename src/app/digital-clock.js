4fe2ffb7eee0131e7e0aaa647730f617d1fd201f
Bishoy Labib <bishoymamdouh@hotmail.com>
2023-10-10 13:37:06 -0700

    UI adjustments

---
+++ src/app/digital-clock.js
@@ -0,0 +1,21 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
+const DigitalClock = () => {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const formattedTime = time.toLocaleTimeString();
+
+  return <div>{formattedTime}</div>;
+};
+
+export default DigitalClock;
