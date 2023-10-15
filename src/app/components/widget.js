69b4b3f614644894c8f9459a7be8ff26ed24f6b1
Bishoy Labib <bishoymamdouh@hotmail.com>
2023-10-14 18:29:11 -0700

    playing with sizing

--- src/app/components/widget.js
+++ src/app/components/widget.js
@@ -1,6 +1,8 @@
-const Widget = ({ children }) => {
+const Widget = ({ className, children }) => {
   return (
-    <div className="square transition ease-in-out hover:scale-105 hover:bg-blue-500 hover:ring-blue-500 hover:shadow-xl group p-3 ring-1 ring-slate-200 bg-white shadow-lg rounded-lg font-sans font-medium text-4xl text-center hover:text-gray-100 text-gray-400">
+    <div
+      className={`${className} hover:col-span-2 hover:row-span-2 square transition ease-in-out hover:scale-105 hover:bg-blue-500 hover:ring-blue-500 hover:shadow-xl group p-3 ring-1 ring-slate-200 bg-white shadow-lg rounded-lg font-sans font-medium text-4xl text-center hover:text-gray-100 text-gray-400`}
+    >
       {children}
     </div>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -6,14 +6,13 @@ export default function Home() {
     <div className="grid bg-gray-100 place-items-center min-h-screen p-5">
       <div className="grid gap-5 max-w-5xl grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
         <Widget>
-          <h1 class="text-4xl font-extrabold">Home</h1>
+          <h1 className="text-4xl font-extrabold">Home</h1>
         </Widget>
         <Widget>1</Widget>
-        <Widget>2</Widget>
+        <Widget className="">2</Widget>
         <Widget>3</Widget>
         <Widget>4</Widget>
         <Widget>5</Widget>
-        <Widget>6</Widget>
         <Widget>
           <DigitalClock />
         </Widget>
